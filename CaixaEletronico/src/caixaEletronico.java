d59392c8403f64685f0af448a977fdad6f0c1ea8
Marydev23 <marilzasantos152@gmail.com>
2024-06-22 00:47:20 -0300

    aterar

--- CaixaEletronico/src/caixaEletronico.java
+++ CaixaEletronico/src/caixaEletronico.java
@@ -1,14 +1,15 @@
 public class caixaEletronico {
     public static void main(String[] args) {
 
-        double saldo = 27.0;
+        double saldo = 30.0;
         double valorSolicitado = 26.0; 
         
         if (valorSolicitado <= saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println(" O saldo é " + saldo);
+            
+        } else {
+            System.out.println("Saldo insuficiente. O saldo é " + saldo);
         }
-
-        System.out.println("O saldo é " + saldo);
     }
 }
-
